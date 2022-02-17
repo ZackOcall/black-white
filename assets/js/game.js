fd51d616a31399f2c97d5a40cf9d8a4bfda76090
Zack O'Callaghan <zocallaghan@googlemail.com>
2022-02-17 19:51:35 +0000

    add eventlisteners for dom and start

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "start") {
+                alert(`Pick a tile!`)
+            }
+        });
+    }
+});
