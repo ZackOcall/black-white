60cb6caf7173d08ea8c3db4663518b92f1a3a069
Zack O'Callaghan <zocallaghan@googlemail.com>
2022-02-17 19:52:57 +0000

    add chooseTile function to js

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,3 +9,25 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
+
+function chooseTile() {
+    if ($(this).hasClass("0")) {
+        computerTurn();
+    } else if ($(this).hasClass("1")) {
+        computerTurn();
+    } else if ($(this).hasClass("2")) {
+        computerTurn();
+    } else if ($(this).hasClass("3")) {
+        computerTurn();
+    } else if ($(this).hasClass("4")) {
+        computerTurn();
+    } else if ($(this).hasClass("5")) {
+        computerTurn();
+    } else if ($(this).hasClass("6")) {
+        computerTurn();
+    } else if ($(this).hasClass("7")) {
+        computerTurn();
+    } else if ($(this).hasClass("8")) {
+        computerTurn();
+    }
+}
